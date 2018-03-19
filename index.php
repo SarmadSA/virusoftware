52f5c8ac27b6e05a6bbf0db01d236e1000da8cd2
Sarmad <sarmad@virusoftware.com>
2018-03-19 10:24:30 +0100

    Added background fade on scroll to header.

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
 <link rel="stylesheet" href="styles/main.css">
 <link rel="stylesheet" href="styles/header.css">
 <link href="https://fonts.googleapis.com/css?family=Abel|Saira+Condensed" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
     <main>
         <section id="image-section">
             <?php include'navigationMenu.php'; ?>
+            <script src="script/header.js" type="text/javascript"></script>
         </section>
     </main>
 </body>
