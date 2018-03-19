573f7e9fde04711b34be42149ada797cabbf4607
Sarmad <sarmad@virusoftware.com>
2018-03-19 12:37:03 +0100

    Added footer template.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <title>Virusoftware</title>
 <link rel="stylesheet" href="styles/main.css">
 <link rel="stylesheet" href="styles/header.css">
+<link rel="stylesheet" href="styles/footer.css">
 <link href="https://fonts.googleapis.com/css?family=Abel|Saira+Condensed" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
@@ -15,6 +16,7 @@
             <?php include'templates/navigationMenu.php'; ?>
             <script src="script/header.js" type="text/javascript"></script>
         </section>
+        <?php include'templates/footer.php';?>
     </main>
 </body>
 </html>
