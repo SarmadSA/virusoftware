2832b18165d12b1feecd2cb1ba3ae9c9bcef5851
Sarmad <sarmad@virusoftware.com>
2018-03-20 15:02:09 +0100

    Added styling to footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,22 @@
+<style type="text/css">
+@import url("../styles/footer.css");
+</style>
 <footer id="footer">
-    
+    <div id="logo-container">
+        <!--SVG logo-->
+        <?php include'virusoftware-logo.svg'; ?>
+        <!--Logo text-->
+        <h1 id="logo-text">Virusoftware</h1>
+    </div>
+    <nav id="footer-nav">
+        <ul id="footer-ul">
+            <li><a href="https://www.google.no/">Home</a></li>
+            <li><a href="https://www.google.no/">Software</a></li>
+            <li><a href="https://www.google.no/">About</a></li>
+            <li><a href="https://www.google.no/">Contact</a></li>
+            <li><a href="https://www.google.no/">Other websites</a></li>
+        </ul>
+    </nav>
+    <p id="development-info"> Developed by virusoftware | <a href="http://www.virusoftware.com">www.virusoftware.com</a></p>
+    <small id="copyright-text">&copy; 2016 - <?php echo date('Y'); ?> virusoftware</small>
 </footer>
