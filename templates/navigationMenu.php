b67c71486faae0ba20117a1a1e12d5be16733fae
Sarmad <sarmad@virusoftware.com>
2018-03-20 15:02:09 +0100

    Added navigationMenu.php to template folder.

---
+++ templates/navigationMenu.php
@@ -0,0 +1,16 @@
+<nav id="nav-menu" class="nav-menu">
+    <!--Logo container-->
+    <a href="../index.php">
+        <div id="logo-container">
+            <!--SVG logo-->
+            <?php include'../virusoftware-logo.svg'; ?>
+            <!--Logo text-->
+            <h1 id="logo-text">Virusoftware</h1>
+        </div>
+    </a>   
+	<ul id="navigation-ul">
+		<li class="list-element"><a href="../index.php">Software</a></li>
+		<li class="list-element"><a href="../about.php">About</a></li> 
+		<li class="list-element"><a href="../contact.php">Contact</a></li> 
+	</ul>
+</nav>
