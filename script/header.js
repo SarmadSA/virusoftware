0c9bbf9a59027703a81b0e92ccb5fbd410397711
Sarmad <sarmad@virusoftware.com>
2018-03-20 15:02:09 +0100

    Added fill logo on scroll.

--- script/header.js
+++ script/header.js
@@ -12,5 +12,15 @@ $(window).scroll(function() {
         opacity = maxOpacity;
     }
     //change the opacity of header background on scroll.
-    $("#nav-menu").css("background-color", backgroundColor);    
+    $("#nav-menu").css("background-color", backgroundColor);
+    
+    //Fill logo when header background opacity > 0.2.
+    if(opacity > 0.2){
+        $(".svg-logo .cls-1").css("fill", "#00FF00");
+        $(".svg-logo .cls-2").css("fill", "#00FF00");
+    }
+    else{
+        $(".svg-logo .cls-1").css("fill", "white");
+        $(".svg-logo .cls-2").css("fill", "white");
+    }
 });
