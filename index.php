ce3233942ded03f26d32e49c8e8eae68d5b92152
Sarmad <sarmad@virusoftware.com>
2018-03-20 15:02:09 +0100

    Added main heading & subscribe to site form.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="styles/main.css">
 <link rel="stylesheet" href="styles/header.css">
 <link rel="stylesheet" href="styles/logo.css">
+<link rel="stylesheet" href="styles/homepage.css">
 <link rel="stylesheet" href="styles/footer.css">
 <link href="https://fonts.googleapis.com/css?family=Abel|Saira+Condensed" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -16,9 +17,17 @@
         <section id="image-section">
             <?php include'templates/navigationMenu.php'; ?>
             <script src="script/header.js" type="text/javascript"></script>
+            <div id="announcement-container">
+                <h1>Offering quality software solutions</h1>
+                <p id="info-text">Virusoftware is currently in pre alpha.<p>
+                <br>              
+                <br>
+                <p>Get the latest news about virusoftware:</p>
+                <?php include'templates/homepageForm.php';?>
+            </div>
         </section>
         <section id="main-section">
-        
+            <h1>Services offered by virusoftware</h1>
         </section>
         <?php include'templates/footer.php';?>
     </main>
---
+++ templates/homepageForm.php
@@ -0,0 +1,9 @@
+<?php
+    //validation of input
+
+?>
+<form action="">
+    <input class="email-input focus-style" type="email" placeholder="Email..">
+    <br>
+    <input class="submit-btn" type="submit">
+</form>
