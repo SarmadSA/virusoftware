b786bba9c28cf934748f72e9f77796db61cb4db9
Sarmad <sarmad@virusoftware.com>
2018-03-20 15:02:09 +0100

    Added connection to stylesheets.

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
 <meta charset="utf-8">
 <title>Virusoftware</title>
+<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="styles/header.css">
+<link href="https://fonts.googleapis.com/css?family=Abel|Saira+Condensed" rel="stylesheet">
 </head>
 
 <body>
