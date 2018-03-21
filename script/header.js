934fff6773696f4224b40cfb80eb9ba07fbfcfa2
Sarmad <sarmad@virusoftware.com>
2018-03-21 20:37:14 +0100

    Fix header fade on scroll.

--- script/header.js
+++ script/header.js
@@ -1,16 +1,40 @@
-var opacity;
+var opacity = 0;
 var maxOpacity = 0.5;
+var scroll = 0;
+
+$( document ).ready(function() {
+    "use strict";
+    if(window.location.pathname === "/virusoftware/index.php" || window.location.pathname === "/virusoftware/"){
+        var backgroundColor = "rgba(0,0,0," + 0 + ")";
+        $("#nav-menu").css("background-color", backgroundColor);   
+    }
+    else{
+        var backgroundColor = "rgba(0,0,0," + 1 + ")";
+        $("#nav-menu").css("background-color", backgroundColor);   
+    }
+    console.log(window.location.pathname);
+});
 
 $(window).scroll(function() {
     "use strict";
-    var scroll = $(window).scrollTop();
+    scroll = $(window).scrollTop();
     var backgroundColor = "rgba(0,0,0," + opacity + ")";
-    if(scroll/1000 <= maxOpacity){
-        opacity = scroll/1000;
+    /*its possible to use window.location.href.indexOf("index.php") > -1
+        when uploading server make sure that this code works
+        i don't think www.virusoftware
+    */
+    if (window.location.pathname === "/virusoftware/index.php" || window.location.pathname === "/virusoftware/") {
+        if(scroll/1000 <= maxOpacity){
+            opacity = scroll/1000;
+        }
+        else{
+            opacity = maxOpacity;
+        }  
     }
     else{
-        opacity = maxOpacity;
+        opacity = 0.7;             
     }
+    
     //change the opacity of header background on scroll.
     $("#nav-menu").css("background-color", backgroundColor);
     
