cd3e9388d901cf0cefce577184d38b40fd67e93f
Sarmad <sarmad@virusoftware.com>
2018-03-23 14:42:07 +0100

    Fix service section scroll animation.

--- script/smoothScroll.js
+++ script/smoothScroll.js
@@ -1,7 +1,19 @@
-$('.services').click(function(){
+var homePaths = ["", "/", "/index.php", "/virusoftware/", "/virusoftware/index.php"];
+
+$('#services').click(function(){
     "use strict";
-    var positionToScroll = $('#service-section').offset().top - $('#nav-menu').height() - 30;
-    $("html, body").animate({scrollTop: positionToScroll}, '500', 'swing');
+    if(isAtHomePage()){
+        console.log("at home page");
+        //add #service-section to herf
+        $("#services").attr("href", "index.php#service-section");
+        var positionToScroll = $('#service-section').offset().top - $('#nav-menu').height() - 30;
+        $("html, body").animate({scrollTop: positionToScroll}, '500', 'swing');
+        //remove #service-section from herf
+    }
+    else{
+        console.log("cookie created");
+        localStorage.setItem("servicesClicked", "true");    
+    }
 });
 
 $('.logo-container').click(function(){
@@ -19,8 +31,20 @@ $('.contact').click(function(){
 
 $(document).ready(function() {
     "use strict";
-    if(window.location.hash === '#service-section'){
+    if(localStorage.getItem("servicesClicked")){
         var positionToScroll = $('#service-section').offset().top - $('#nav-menu').height() - 30;
         $("html, body").animate({scrollTop: positionToScroll}, '500', 'swing');
+        localStorage.removeItem("servicesClicked");
     }
 });
+
+function isAtHomePage(){
+    "use strict";
+    var isAtHomePage = false;
+    homePaths.forEach(function(element){
+        if(window.location.pathname === element){
+            isAtHomePage = true;
+        }
+    });
+    return isAtHomePage;
+}
